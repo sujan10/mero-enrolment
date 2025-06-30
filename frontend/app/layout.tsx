109bd343b682334cbf58bb791bf82470ad6c6499
Sujan Khanal <sujankhanal@Sujans-MacBook-Pro.local>
2025-06-30 21:53:09 +1000

    chore: re-add app files for deployment (main)

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,9 @@
+import "../globals.css";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="bg-white text-gray-900">{children}</body>
+    </html>
+  );
+} 
