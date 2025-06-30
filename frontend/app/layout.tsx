bf2ec78d632d328b161ff7ec3d089d1a1e5e05e2
Sujan Khanal <sujankhanal@Sujans-MacBook-Pro.local>
2025-06-30 21:53:09 +1000

    chore: remove app files for reset (main)

--- frontend/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "../globals.css";
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className="bg-white text-gray-900">{children}</body>
-    </html>
-  );
-} 
